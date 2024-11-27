8322c36615c8ae4336d5bf4593746817a77bb91c
KaeltecQA <nascimentopereiracesarkael@gmail.com>
2024-11-27 20:51:16 -0300

    aulaIJJ

--- features/py.feature
+++ features/py.feature
@@ -1,8 +1,7 @@
 Feature: Formulario de Contato
 
-
   Scenario: Enviar Mensagem
     Given Entro na Página de contato do Instituto Joga Junto
      When Insiro meus dados
-      And "Envio a mensagem "Teste - Oi!"
-      Then Fechar o navegador
+      When "Envio a mensagem "Teste - Oi!"
+       When Clico no botão enviar
--- features/steps/steps.py
+++ features/steps/steps.py
@@ -2,6 +2,9 @@ from behave import given, when, then
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 @given(u'Entro na Página de contato do Instituto Joga Junto')
 def step_impl(context):
@@ -11,27 +14,21 @@ def step_impl(context):
 
 @when(u'Insiro meus dados')
 def step_impl(context):
-    context.driver.find_element(By.ID , "nome").send_keys("Cesar Kael")
-    context.driver.find_element(By.ID , "email").send_keys("Kael@teste.com")
-    context.driver.find_element(By.ID , "assunto").send_keys(Keys.ARROW_DOWN)
-    context.driver.find_element(By.ID , "assunto").send_keys(Keys.ARROW_DOWN)
-    context.driver.find_element(By.ID , "assunto").send_keys(Keys.ARROW_DOWN)
-    context.driver.find_element(By.ID , "assunto").send_keys(Keys.ARROW_DOWN)
-    context.driver.find_element(By.ID , "assunto").send_keys(Keys.ARROW_DOWN)
-    context.driver.find_element(By.ID , "assunto").send_keys(Keys.ARROW_DOWN)
-    context.driver.find_element(By.ID , "assunto").send_keys(Keys.ARROW_DOWN , Keys.TAB)
-
+    context.driver.find_element(By.ID , "nome").send_keys("Kael")
+    context.driver.find_element(By.ID , "email").send_keys("ckael0210@teste.com")
+    select = context.driver.find_element(By.ID, "assunto") 
+    for _ in range(7): 
+        select.send_keys(Keys.ARROW_DOWN)
+    select.send_keys(Keys.TAB)
 
 @when(u'"Envio a mensagem "Teste - Oi!"')
 def step_impl(context):
     context.driver.find_element(By.ID, "mensagem").send_keys("Teste - Oi!")
 
-@when(u'Clico no botão de envio')
-def step_impl(context):
-    context.driver.find_element(By.XPATH, '//*[@id="Contato"]/div[1]/form/button').click()
-    context.driver.click()
+    botao_enviar = WebDriverWait(context.driver, 6).until(
+        EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[2]/section[9]/div[1]/form/button"))
+    )
+    context.driver.execute_script("arguments[0].click();", botao_enviar)
 
-    
-@then (u"Fechar o navegador")
-def step_impl(context):
-  context.driver.quit()
+
+
